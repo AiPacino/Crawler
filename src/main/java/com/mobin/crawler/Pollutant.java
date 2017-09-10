303d621806172ef40cb553254f36fee9ad62b0da
mobin <996833461@qq.com>
2017-09-10 21:39:10 +0800

    过去24小时污染物浓度

--- src/main/java/com/mobin/crawler/Pollutant.java
+++ src/main/java/com/mobin/crawler/Pollutant.java
@@ -4,16 +4,20 @@ import com.mobin.config.Config;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Html;
 
 /**
  * Created by Mobin on 2017/9/8.
+ * 过去24小时污染物浓度
  */
 public class Pollutant implements PageProcessor{
     private Site site = Site.me().setRetryTimes(3).setTimeOut(1000);
     @Override
     public void process(Page page) {
-        System.out.println(page.getHtml().xpath("//table[@class='tblNormal']//td"));
-       // System.out.println(page.getHtml().css("table.tblNormal").$("th").xpath(""));
+        Html html = page.getHtml();
+        for (int i = 1; i<= 13; i ++) {
+            System.out.println(html.xpath("//table[@class='tblNormal']/tbody/tr["+i+"]/allText()"));
+        }
     }
 
     @Override
